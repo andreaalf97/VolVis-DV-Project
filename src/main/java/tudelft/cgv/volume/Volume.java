a30d97bf19dcd73b192441227cddc83c98cfa0d4
Reinier Koops <info@reinier.work>
2020-01-11 17:38:33 +0100

    Formatting & commenting of code changed
    This is according recommended style guide.

--- src/main/java/tudelft/cgv/volume/Volume.java
+++ src/main/java/tudelft/cgv/volume/Volume.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- *
  * @author michel modified by Anna
  */
 
@@ -17,7 +16,7 @@ import java.io.IOException;
 //////////////////////////////////////////////////////////////////////
 
 public class Volume {
-    
+
 
     //Do NOT modify these attributes
     private int dimX, dimY, dimZ;
@@ -27,84 +26,78 @@ public class Volume {
     // Do NOT modify this function
     // This function returns the nearest neighbour given a position in the volume given by coord.
     public short getVoxelNN(double[] coord) {
-        if (coord[0] < 0 || coord[0] > (dimX-1) || coord[1] < 0 || coord[1] > (dimY-1)
-                || coord[2] < 0 || coord[2] > (dimZ-1)) {
+        if (coord[0] < 0 || coord[0] > (dimX - 1) || coord[1] < 0 || coord[1] > (dimY - 1)
+                || coord[2] < 0 || coord[2] > (dimZ - 1)) {
             return 0;
         }
         /* notice that in this framework we assume that the distance between neighbouring voxels is 1 in all directions*/
-        int x = (int) Math.round(coord[0]); 
+        int x = (int) Math.round(coord[0]);
         int y = (int) Math.round(coord[1]);
         int z = (int) Math.round(coord[2]);
-    
+
         return getVoxel(x, y, z);
     }
-        
+
 
     //Do NOT modify this function
     //This function linearly interpolates the value g0 and g1 given the factor (t) 
     //the result is returned. It is used for the tri-linearly interpolation the values 
     private float interpolate(float g0, float g1, float factor) {
-        float result = (1 - factor)*g0 + factor*g1;
-        return result; 
+        float result = (1 - factor) * g0 + factor * g1;
+        return result;
     }
-             
+
     //Do NOT modify this function
     // This function returns the trilinear interpolated value of the position given by  position coord.
     public float getVoxelLinearInterpolate(double[] coord) {
-        if (coord[0] < 0 || coord[0] > (dimX-2) || coord[1] < 0 || coord[1] > (dimY-2)
-                || coord[2] < 0 || coord[2] > (dimZ-2)) {
+        if (coord[0] < 0 || coord[0] > (dimX - 2) || coord[1] < 0 || coord[1] > (dimY - 2)
+                || coord[2] < 0 || coord[2] > (dimZ - 2)) {
             return 0;
         }
         /* notice that in this framework we assume that the distance between neighbouring voxels is 1 in all directions*/
-        int x = (int) Math.floor(coord[0]); 
+        int x = (int) Math.floor(coord[0]);
         int y = (int) Math.floor(coord[1]);
         int z = (int) Math.floor(coord[2]);
-        
+
         float fac_x = (float) coord[0] - x;
         float fac_y = (float) coord[1] - y;
         float fac_z = (float) coord[2] - z;
 
-        float t0 = interpolate(getVoxel(x, y, z), getVoxel(x+1, y, z), fac_x);
-        float t1 = interpolate(getVoxel(x, y+1, z), getVoxel(x+1, y+1, z), fac_x);
-        float t2 = interpolate(getVoxel(x, y, z+1), getVoxel(x+1, y, z+1), fac_x);
-        float t3 = interpolate(getVoxel(x, y+1, z+1), getVoxel(x+1, y+1, z+1), fac_x);
+        float t0 = interpolate(getVoxel(x, y, z), getVoxel(x + 1, y, z), fac_x);
+        float t1 = interpolate(getVoxel(x, y + 1, z), getVoxel(x + 1, y + 1, z), fac_x);
+        float t2 = interpolate(getVoxel(x, y, z + 1), getVoxel(x + 1, y, z + 1), fac_x);
+        float t3 = interpolate(getVoxel(x, y + 1, z + 1), getVoxel(x + 1, y + 1, z + 1), fac_x);
         float t4 = interpolate(t0, t1, fac_y);
         float t5 = interpolate(t2, t3, fac_y);
         float t6 = interpolate(t4, t5, fac_z);
-        
-        return t6; 
+
+        return t6;
     }
 
+    // User defined; recommended values are: -1, -0.75, -0.5
+    float a = -0.75f; // global variable <a> used in cubic interpolation.
 
-    float a = -0.75f; // global variable that defines the value of a used in cubic interpolation.
-    // you need to chose the right value
-        
-    //////////////////////////////////////////////////////////////////////
-    ///////////////// FUNCTION TO BE IMPLEMENTED /////////////////////////
-    ////////////////////////////////////////////////////////////////////// 
-        
     // Function that computes the weights for one of the 4 samples involved in the 1D interpolation
-    // Weight(x) is basically h(x)
-    public float weight(float x)
-    {
+    // weight(x) is h(x): the Cubic interpolation kernel
+    public float weight(float x) {
         float abs = Math.abs(x);
 
-        if(abs < 1)
-            return (float)(((a + 2)*Math.pow(abs, 3)) - ((a + 3)*Math.pow(abs, 2)) + 1);
+        if (abs < 1)
+            return (float) (((a + 2) * Math.pow(abs, 3)) - ((a + 3) * Math.pow(abs, 2)) + 1);
 
-        if(abs >= 1 && abs < 2)
-            return (float)((a * Math.pow(abs, 3)) - (5 * a * Math.pow(abs, 2)) + (8 * a * abs) - (4 * a));
+        if (abs >= 1 && abs < 2)
+            return (float) ((a * Math.pow(abs, 3)) - (5 * a * Math.pow(abs, 2)) + (8 * a * abs) - (4 * a));
 
         return 0.0f;
     }
-    
+
     //////////////////////////////////////////////////////////////////////
     ///////////////// FUNCTION TO BE IMPLEMENTED /////////////////////////
     ////////////////////////////////////////////////////////////////////// 
     // Function that computes the 1D cubic interpolation. g0,g1,g2,g3 contain the values of the voxels that we want to interpolate
     // factor contains the distance from the value g1 to the position we want to interpolate to.
     // We assume the out of bounce checks have been done earlier
-    
+
     public float cubicinterpolate(float g0, float g1, float g2, float g3, float factor) {
         float result = 0.0f;
 
@@ -112,27 +105,27 @@ public class Volume {
         result += g1 * weight(factor);
         result += g2 * weight(1 - factor);
         result += g3 * weight(2 - factor);
-      
-        return result; 
+
+        return result;
     }
-        
+
     //////////////////////////////////////////////////////////////////////
     ///////////////// FUNCTION TO BE IMPLEMENTED /////////////////////////
     ////////////////////////////////////////////////////////////////////// 
     // 2D cubic interpolation implemented here. We do it for plane XY. Coord contains the position.
     // We assume the out of bounce checks have been done earlier
-    public float bicubicinterpolateXY(double[] coord,int z) {
+    public float bicubicinterpolateXY(double[] coord, int z) {
 
         //Coord is an array in the form (x_, y_)
-        int x = (int)Math.floor(coord[0]);
-        int y = (int)Math.floor(coord[1]);
+        int x = (int) Math.floor(coord[0]);
+        int y = (int) Math.floor(coord[1]);
 
         float t0 = cubicinterpolate(
                 getVoxel(x - 1, y - 1, z),
                 getVoxel(x, y - 1, z),
                 getVoxel(x + 1, y - 1, z),
                 getVoxel(x + 2, y - 1, z),
-                (float)(coord[0] - x)
+                (float) (coord[0] - x)
         );
 
         float t1 = cubicinterpolate(
@@ -140,7 +133,7 @@ public class Volume {
                 getVoxel(x, y, z),
                 getVoxel(x + 1, y, z),
                 getVoxel(x + 2, y, z),
-                (float)(coord[0] - x)
+                (float) (coord[0] - x)
         );
 
         float t2 = cubicinterpolate(
@@ -148,7 +141,7 @@ public class Volume {
                 getVoxel(x, y + 1, z),
                 getVoxel(x + 1, y + 1, z),
                 getVoxel(x + 2, y + 1, z),
-                (float)(coord[0] - x)
+                (float) (coord[0] - x)
         );
 
         float t3 = cubicinterpolate(
@@ -156,39 +149,39 @@ public class Volume {
                 getVoxel(x, y + 2, z),
                 getVoxel(x + 1, y + 2, z),
                 getVoxel(x + 2, y + 2, z),
-                (float)(coord[0] - x)
+                (float) (coord[0] - x)
         );
 
-        return cubicinterpolate(t0, t1, t2, t3, (float)(coord[1] - y));
+        return cubicinterpolate(t0, t1, t2, t3, (float) (coord[1] - y));
 
     }
-            
+
     //////////////////////////////////////////////////////////////////////
     ///////////////// FUNCTION TO BE IMPLEMENTED /////////////////////////
     ////////////////////////////////////////////////////////////////////// 
     // 3D cubic interpolation implemented here given a position in the volume given by coord.
-    
+
     public float getVoxelTriCubicInterpolate(double[] coord) {
-        if (coord[0] < 1 || coord[0] > (dimX-3) || coord[1] < 1 || coord[1] > (dimY-3)
-                || coord[2] < 1 || coord[2] > (dimZ-3)) {
+        if (coord[0] < 1 || coord[0] > (dimX - 3) || coord[1] < 1 || coord[1] > (dimY - 3)
+                || coord[2] < 1 || coord[2] > (dimZ - 3)) {
             return 0;
         }
-      
+
         //coord is like [x_, y_, z_]
 
-        int z = (int)Math.floor(coord[2]);
+        int z = (int) Math.floor(coord[2]);
 
         float t0 = bicubicinterpolateXY(coord, z - 1);
         float t1 = bicubicinterpolateXY(coord, z);
         float t2 = bicubicinterpolateXY(coord, z + 1);
         float t3 = bicubicinterpolateXY(coord, z + 2);
 
-        float result = cubicinterpolate(t0, t1, t2, t3, (float)Math.abs(coord[2] - z));
+        float result = cubicinterpolate(t0, t1, t2, t3, (float) Math.abs(coord[2] - z));
 
         //Without using this if sequence, there were while lines around the border of the objects
-        if(result < 0)
+        if (result < 0)
             return 0;
-        if(result > 255)
+        if (result > 255)
             return 255;
 
         return result;
@@ -199,17 +192,18 @@ public class Volume {
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-	
+
     //Do NOT modify this function
     public Volume(int xd, int yd, int zd) {
-        data = new short[xd*yd*zd];
+        data = new short[xd * yd * zd];
         dimX = xd;
         dimY = yd;
         dimZ = zd;
     }
+
     //Do NOT modify this function
     public Volume(File file) {
-        
+
         try {
             VolumeIO reader = new VolumeIO(file);
             dimX = reader.getXDim();
@@ -220,41 +214,41 @@ public class Volume {
         } catch (IOException ex) {
             System.out.println("IO exception");
         }
-        
+
     }
-    
+
     //Do NOT modify this function
     public short getVoxel(int x, int y, int z) {
-    	int i = x + dimX*(y + dimY * z);
+        int i = x + dimX * (y + dimY * z);
         return data[i];
     }
-    
+
     //Do NOT modify this function
     public void setVoxel(int x, int y, int z, short value) {
-    	int i = x + dimX*(y + dimY * z);
+        int i = x + dimX * (y + dimY * z);
         data[i] = value;
     }
-    
-	//Do NOT modify this function
+
+    //Do NOT modify this function
     public void setVoxel(int i, short value) {
         data[i] = value;
     }
-    
+
     //Do NOT modify this function
     public short getVoxel(int i) {
         return data[i];
     }
-    
-	//Do NOT modify this function
+
+    //Do NOT modify this function
     public int getDimX() {
         return dimX;
     }
-    
+
     //Do NOT modify this function
     public int getDimY() {
         return dimY;
     }
-    
+
     //Do NOT modify this function
     public int getDimZ() {
         return dimZ;
@@ -263,30 +257,30 @@ public class Volume {
     //Do NOT modify this function
     public short getMinimum() {
         short minimum = data[0];
-        for (int i=0; i<data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             minimum = data[i] < minimum ? data[i] : minimum;
         }
         return minimum;
     }
-    
+
     //Do NOT modify this function
     public short getMaximum() {
         short maximum = data[0];
-        for (int i=0; i<data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             maximum = data[i] > maximum ? data[i] : maximum;
         }
         return maximum;
     }
- 
+
     //Do NOT modify this function
     public int[] getHistogram() {
         return histogram;
     }
-    
+
     //Do NOT modify this function
     private void computeHistogram() {
         histogram = new int[getMaximum() + 1];
-        for (int i=0; i<data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             histogram[data[i]]++;
         }
     }
